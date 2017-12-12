d7e2171f4cf18cb6166f1aabe9a5629ef58968e5
chilyard <chilyard@localhost.localdomain>
2017-12-11 21:26:36 -0800

    updates

--- test/test_github_token.py
+++ test/test_github_token.py
@@ -9,13 +9,20 @@ class TestGithubToken(unittest.TestCase):
   def setUp(self):
     self.token = github_token.GithubToken()
 
-  def test_check_environment_variable(self):
+  def test_check_environment_variable_is_unset(self):
     self.assertIsNone(self.token.check_environment_var())
 
-  def test_set_token_in_env(self):
-    self.assertIsNotNone(self.token.set_environment_var())
+  def test_check_environment_variable_is_set(self):
     token = self.token.check_environment_var()
     self.assertRegex(token, "^[a-z0-9]{39,42}")
+
+  #def test_set_token_in_env(self):
+  #  self.assertIsNotNone(self.token.set_environment_var())
+  #  token = self.token.check_environment_var()
+  #  self.assertRegex(token, "^[a-z0-9]{39,42}")
+
+  def test_get_token_from_user(self):
+    pass
 
 
 if __name__ == '__main__':
