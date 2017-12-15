6c84c7bf71ed52ba92df595b8b8e25c314c9e2b0
Chuck Hilyard <chuck.hilyard@reachlocal.com>
2017-12-15 15:53:58 -0800

    sorta working

--- test/test_github_token.py
+++ test/test_github_token.py
@@ -3,6 +3,7 @@ import unittest
 import github_token
 from github_token import github_token
 import re
+import os
 
 
 class TestGithubToken(unittest.TestCase):
@@ -14,7 +15,7 @@ class TestGithubToken(unittest.TestCase):
     gt = self.token.check_environment_var()
     self.assertIsNone(gt)
 
-  @unittest.skipUnless(gt, "gt is unset")
+  @unittest.skipUnless(os.getenv('GITHUB_TOKEN'), "gt is unset")
   def test_check_environment_variable_is_set(self):
     gt = self.token.check_environment_var()
     gtre = re.compile("^[a-z0-9]{39,42}")
